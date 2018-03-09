83f51e554adc8cdb0b0b27f884a6ad25d05e0e19
Mike Blouin <mblouin@microsoft.com>
2018-03-08 21:59:36 -0800

    fix(best-practices): Best practices and simplify
    Added best practices:
    	- init on window.onload: Controls should wait for onload to call
    	`mixer.isLoaded()`
    	- call `mixer.isloaded()`
    
    Also doesn't move the video every second to simplify sample and remove
    distraction.

--- src/scripts.js
+++ src/scripts.js
@@ -1,24 +1,22 @@
-// Every second, move the video around! Note: this won't work on
-// mobile, you can't move the video around there.
-setInterval(function () {
-    mixer.display.moveVideo({
-        top: Math.random() * 100,
-        bottom: Math.random() * 100,
-        left: Math.random() * 100,
-        right: Math.random() * 100,
-    });
-}, 1000);
+window.onload = function initMixer() {
+    // Move the video by a static offset amount
+    const offset = 50;
+	mixer.display.moveVideo({
+		top: offset,
+		bottom: offset,
+		left: offset,
+		right: offset,
+	});
 
-// Whenever someone clicks on "Hello World", we'll send an event
-// to the game client on the control ID "title"
-document.getElementById('title').onclick = function (event) {
-    mixer.socket.call('giveInput', {
-        controlID: 'title',
-        event: 'click',
-        button:  event.button,
-    });
-};
+	// Whenever someone clicks on "Hello World", we'll send an event
+	// to the game client on the control ID "title"
+	document.getElementById('title').onclick = function (event) {
+		mixer.socket.call('giveInput', {
+			controlID: 'title',
+            event: 'click',
+            button: event.button,
+		});
+    };
 
-// You can read a full set of docs for the `mixer` library at:
-// https://<todo>
-// Talk to us on Discord if you have any questions! discord.gg/mixer
+	mixer.isLoaded();
+}
