c33ea84419c184c2ee42174843f19163607876cd
Mike Blouin <mblouin@microsoft.com>
2018-03-12 12:51:38 -0700

    fix(best-practice): PR Feedback
    Prettify files and add npm scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,22 @@
-window.onload = function initMixer() {
-    // Move the video by a static offset amount
-    const offset = 50;
-	mixer.display.moveVideo({
-		top: offset,
-		bottom: offset,
-		left: offset,
-		right: offset,
-	});
+window.addListener('load', function initMixer() {
+  // Move the video by a static offset amount
+  const offset = 50;
+  mixer.display.moveVideo({
+    top: offset,
+    bottom: offset,
+    left: offset,
+    right: offset,
+  });
 
-	// Whenever someone clicks on "Hello World", we'll send an event
-	// to the game client on the control ID "title"
-	document.getElementById('title').onclick = function (event) {
-		mixer.socket.call('giveInput', {
-			controlID: 'title',
-            event: 'click',
-            button: event.button,
-		});
-    };
+  // Whenever someone clicks on "Hello World", we'll send an event
+  // to the game client on the control ID "title"
+  document.getElementById('title').onclick = function(event) {
+    mixer.socket.call('giveInput', {
+      controlID: 'title',
+      event: 'click',
+      button: event.button,
+    });
+  };
 
-	mixer.isLoaded();
-}
+  mixer.isLoaded();
+});
